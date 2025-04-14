06ece4bed854b3607478bfb7bb65bc2147e0ab2b
Erica Thompson <younique98@gmail.com>
2025-04-14 06:28:55 -0400

    wip
    wip: lighthouse rating now 91!
    
    wip: optimizing photos
    
    wip: score now 92
    
    updated lighthouse pdf

--- next.config.js
+++ next.config.js
@@ -1,23 +1,52 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  productionBrowserSourceMaps: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-          },
-      {
-        protocol: "https",
-        hostname: "air-prod.imgix.net",
-          },
-      {
-        protocol: "https",
-        hostname: "air-original.imgix.net",
-          }
-    ],
+    reactStrictMode: true,
+    swcMinify: true,
+    productionBrowserSourceMaps: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+            },
+            {
+                protocol: 'https',
+                hostname: 'air-prod.imgix.net',
+            },
+            {
+                protocol: 'https',
+                hostname: 'air-original.imgix.net',
+            },
+        ],
+    },
+    headers: async () => {
+        return [
+            {
+                source: '/_next/image',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable',
+                    },
+                ],
+            },
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'header',
+                        key: 'accept',
+                        value: 'application/pdf',
+                    },
+                ],
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable',
+                    },
+                ],
+            },
+        ]
     },
 }
 
